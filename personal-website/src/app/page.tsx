210259fffb928a58daaaf6f6f1d677f4c7952c47
William Yang <willi64645@gmail.com>
2025-05-22 16:45:30 -0400

    feat: splashscreen along with loading

--- personal-website/src/app/page.tsx
+++ personal-website/src/app/page.tsx
@@ -1,7 +1,35 @@
+"use client";
+import { useEffect, useState } from "react";
+import SplashScreen from "../components/SplashScreen";
+
 export default function Home() {
-  return (
-      <main className="flex items-center justify-center min-h-screen">
-        <h1 className="text-4xl text-airbus-green">HELLO WORLD</h1>
-      </main>
-  );
+    const [loading, setLoading] = useState(
+        typeof window === "undefined"
+            ? false
+            : !localStorage.getItem("hasSeenSplash")
+    );
+
+    useEffect(() => {
+        const hasSeenSplash = localStorage.getItem("hasSeenSplash");
+        if (!hasSeenSplash) {
+            setLoading(true);
+            const timer = setTimeout(() => {
+                setLoading(false);
+                localStorage.setItem("hasSeenSplash", "true");
+            }, 3000);
+            return () => clearTimeout(timer);
+        } else {
+            setLoading(false);
+        }
+    }, []);
+
+    if (loading) {
+        return <SplashScreen />;
+    }
+
+    return (
+        <main className="flex items-center justify-center min-h-screen">
+            <h1 className="text-4xl">HELLO WORLD</h1>
+        </main>
+    );
 }
---
+++ personal-website/src/components/SplashScreen.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function SplashScreen() {
+    return (
+        <main className="flex items-center justify-center min-h-screen">
+            <h1 className="text-4xl text-airbus-green">LOADING WEBSITE<br />(MAX 3 SECONDS)</h1>
+        </main>
+    );
+}

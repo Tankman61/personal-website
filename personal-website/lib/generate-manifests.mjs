5531fc3e44e4de0ec6c521ccaa901a5d4b54123d
William Yang <willi64645@gmail.com>
2025-08-29 00:38:45 -0400

    fixes

--- personal-website/lib/generate-manifests.mjs
+++ personal-website/lib/generate-manifests.mjs
@@ -1,5 +1,5 @@
-const fs = require('fs');
-const path = require('path');
+import * as fs from 'fs';
+import * as path from 'path';
 
 const assetsDir = path.join(process.cwd(), 'public/assets/images/gallery');
 const manifestPath = path.join(process.cwd(), 'src/lib/gallery-manifest.json');

6e1c09db3d1d64b48f3353309af16bd1f170e023
William Yang <willi64645@gmail.com>
2025-08-28 23:41:55 -0400

    fix for mdx build fail

--- personal-website/next.config.ts
+++ personal-website/next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
+    pageExtensions: ['ts', 'tsx', 'js', 'jsx'],
 };
 
 export default nextConfig;

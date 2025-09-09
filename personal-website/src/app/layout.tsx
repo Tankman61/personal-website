927c38914d3039594cf97fbdcebaf938d80ddbb7
William Yang <willi64645@gmail.com>
2025-09-09 18:36:46 -0400

    add google tag

--- personal-website/src/app/layout.tsx
+++ personal-website/src/app/layout.tsx
@@ -5,6 +5,7 @@ import Navbar from '@/components/Navbar';
 import { SplashScreenWrapper } from '@/components/SplashScreenWrapper';
 import Footer from '@/components/Footer';
 import ScalingWrapper from '@/components/ScalingWrapper';
+import Script from 'next/script';
 
 export const metadata: Metadata = {
   title: 'William Yang',
@@ -14,11 +15,23 @@ export const metadata: Metadata = {
   },
 };
 
-
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body>
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-198STVPVYX"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-198STVPVYX');
+          `}
+        </Script>
         <SplashScreenWrapper>
           <ScalingWrapper>
             <main className="flex flex-col items-center min-h-screen relative z-10 selection:bg-yellow-200 dark:selection:bg-yellow-800">
